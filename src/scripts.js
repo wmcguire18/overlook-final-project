1905e1dfa83471d9f5ed9b9105236d6aa65f1652
William McGuire <wmcguire18@gmail.com>
2021-09-29 08:51:54 -0400

    Initial branch commit, Add imports

--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,11 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import { getAllCustomers, getAllRooms, getAllBookings, addNewBooking } from './apiCalls';
+import Customer from './classes/Customer.js';
+import Room from './classes/Room.js';
+import Booking from './classes/Booking.js';
+import domUpdates from './domUpdates';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
-
-console.log('This is the JavaScript entry file - your code begins here.');
