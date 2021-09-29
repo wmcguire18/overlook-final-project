33794feb47f42556cdd1ffd7aacc329701b66945
William McGuire <wmcguire18@gmail.com>
2021-09-29 09:13:30 -0400

    Add fetch calls on load

--- src/scripts.js
+++ src/scripts.js
@@ -11,3 +11,38 @@ import domUpdates from './domUpdates';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+
+let fetchedCustomers = [];
+let fetchedRooms = [];
+let fetchedBookings = [];
+
+window.addEventListener('load', getData);
+
+function getData() {
+  let fetchedData = Promise.all(
+    [
+      getAllCustomers(),
+      getAllRooms(),
+      getAllBookings(),
+    ]
+  )
+  .then( data => parseData(data))
+  .catch(error => console.warn(error))
+}
+
+function parseData(data) {
+  let customerData = data[0].customers;
+  customerData.forEach(customer => {
+    fetchedCustomers.push(customer);
+  })
+  let roomData = data[1].rooms;
+  roomData.forEach(room => {
+    fetchedRooms.push(room);
+  })
+  let bookingData = data[2].bookings;
+  bookingData.forEach(booking => {
+    fetchedBookings.push(booking);
+  })
+console.log(data);
+return
+};
